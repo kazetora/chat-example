744c5ba15f38b972ad2ce61ed735934b7145f46b
Pratama Putra <putra@nakao-lab.org>
2017-12-11 07:20:07 +0000

    add log rotate

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var today = new Date();
+var todaystr = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+today = today.getFullYew
+var log_rotate = function(){
+    var date = new Date();
+    var datestr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    if(datestr !== todaystr){
+        todaystr = datestr;
+        var exec = require('child-process').exec;
+              
+        var cmd = '/bin/cp /logs/app.log /logs/app.log-' + todaystr;
+        var logrotate = exec(cmd, function(err, stdout, stderr){
+            setTimeout(log_rotate, 1000); 
+        }
+    }
+    else{
+       setTimeout(log_rotate, 1000);
+    }
+    
+}
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log(msg);
@@ -19,4 +41,5 @@ io.on('connection', function(socket){
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
+  log_rotate();
 });
