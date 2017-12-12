676b9f98e673968f99b1ed995742c276aa028822
root <pratama.x.putra@gmail.com>
2017-12-12 00:32:01 +0000

    add log rotate per day

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use('/log', express.static('/logs'));
+var fs = require('fs');
+log_file = fs.createWriteStream(__dirname + '/logs/lora.log', {flags: 'w'});
+
+app.use('/log', express.static('logs'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,16 +18,21 @@ var todaystr = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.
 
 today = today.getFullYew
 var log_rotate = function(){
+    //log_file.once("drain", function(){
+    //    console.log("write to file");
+    //});
     var date = new Date();
     var datestr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     if(datestr !== todaystr){
-        todaystr = datestr;
-        var exec = require('child-process').exec;
+        log_file.end();
+        var exec = require('child_process').exec;
               
-        var cmd = '/bin/cp /logs/app.log /logs/app.log-' + todaystr;
+        var cmd = '/bin/mv ./logs/lora.log ./logs/lora-' + todaystr + '.log';
+        todaystr = datestr;
         var logrotate = exec(cmd, function(err, stdout, stderr){
+            log_file = fs.createWriteStream(__dirname + '/logs/lora.log', {flags: 'w'});
             setTimeout(log_rotate, 1000); 
-        }
+        });
     }
     else{
        setTimeout(log_rotate, 1000);
@@ -35,6 +43,7 @@ var log_rotate = function(){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log(msg);
+    log_file.write(msg + "\n"); 
     io.emit('chat message', msg);
   });
 });
